9fc1725d928838db4c98dd4d0e1695d926a9a324
Aavez <mdaavez3@gmail.com>
2025-09-02 12:42:32 -0700

    update backend

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const errorMiddleware=require ("./middlewares/error-middleware");
 
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://gigswap-hsp-client.onrender.com",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
