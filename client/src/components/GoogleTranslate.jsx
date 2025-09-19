67219b411e9fcd723a86fb44cdd123639dc0a1e5
Aavez <mdaavez3@gmail.com>
2025-09-19 04:30:06 -0700

    para change

--- client/src/components/GoogleTranslate.jsx
+++ client/src/components/GoogleTranslate.jsx
@@ -1,12 +1,46 @@
 // src/components/GoogleTranslate.jsx
 
-import React from 'react';
-import './GoogleTranslate.css'; // Apni CSS file import karein
+import React, { useEffect } from 'react';
+import './GoogleTranslate.css';
 
 export const GoogleTranslate = () => {
+
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement({
+      pageLanguage: 'en'
+    }, 'google_translate_element');
+  };
+
+  const changeLanguage = (lang) => {
+    const select = document.querySelector('.goog-te-combo');
+    if (select) {
+      select.value = lang;
+      select.dispatchEvent(new Event('change'));
+    }
+  };
+
+  useEffect(() => {
+    const addScript = document.createElement('script');
+    addScript.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    addScript.async = true;
+    document.body.appendChild(addScript);
+
+    window.googleTranslateElementInit = googleTranslateElementInit;
+
+    return () => {
+      document.body.removeChild(addScript);
+      delete window.googleTranslateElementInit;
+    };
+  }, []);
+
   return (
     <div id="google_translate_element_container">
       <div id="google_translate_element"></div>
+      <div>
+        <button onClick={() => changeLanguage('en')}>English</button>
+        <button onClick={() => changeLanguage('mr')}>Marathi</button>
+        <button onClick={() => changeLanguage('hi')}>Hindi</button>
+      </div>
     </div>
   );
 };
