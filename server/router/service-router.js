62ad2d61a3c24ae8be627da17b1613cfaaab30ba
MohammadParvez7 <parvezraza712@gmail.com>
2025-12-27 23:16:57 -0800

    Add backend

--- server/router/service-router.js
+++
@@ -1,7 +0,0 @@
-const express =require("express");
-const router=express.Router();
-const services=require("../controllers/service-controller")
-
-router.route("/service").get(services);
-
-module.exports=router;
