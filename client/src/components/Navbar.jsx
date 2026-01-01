922abaf1e29825c6c0f0fd42059a85c7f751b4c0
MohammadParvez <parvezraza712@gmail.com>
2026-01-01 05:45:46 -0800

    worker dashboard added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,22 +8,28 @@ import { FaBars, FaTimes } from "react-icons/fa";
 export const Navbar = () => {
   const { isLoggedIn, user, isLoading } = useAuth();
   const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   const toggleMenu = () => setSidebarOpen(!isSidebarOpen);
 
-  // // Don't render the navbar until user data is loaded
+  // Optional: wait until auth data loads
   // if (isLoading) {
-  //   return null; // or a loading spinner
+  //   return null;
   // }
 
   return (
     <header>
       <div className="container navbar">
+        {/* Logo */}
         <div className="logo-brand">
           <NavLink to="/">GigSwap</NavLink>
         </div>
+
+        {/* Mobile Toggle */}
         <div className="menu-toggle" onClick={toggleMenu}>
           {isSidebarOpen ? <FaTimes /> : <FaBars />}
         </div>
+
+        {/* Navigation */}
         <nav
           className={isSidebarOpen ? "active" : ""}
           onClick={() => setSidebarOpen(false)}
@@ -32,12 +38,16 @@ export const Navbar = () => {
             <li>
               <NavLink to="/">Home</NavLink>
             </li>
+
             <li>
               <NavLink to="/about">About</NavLink>
             </li>
+
             <li>
               <NavLink to="/service">Services</NavLink>
             </li>
+
+            {/* Book */}
             {isLoggedIn ? (
               <li>
                 <NavLink to="/contact">Book</NavLink>
@@ -47,11 +57,22 @@ export const Navbar = () => {
                 <NavLink to="/login">Book</NavLink>
               </li>
             )}
+
+            {/* Admin Panel */}
             {isLoggedIn && user?.isAdmin && (
               <li>
                 <NavLink to="/admin/users">Admin Panel</NavLink>
               </li>
             )}
+
+            {/* Worker Panel */}
+            {isLoggedIn && user?.isWorker && (
+              <li>
+                <NavLink to="/worker/contacts">Worker Panel</NavLink>
+              </li>
+            )}
+
+            {/* Auth Links */}
             {isLoggedIn ? (
               <li>
                 <NavLink to="/logout">Logout</NavLink>
